5401b13dd6a3e97a8578141d06bb3dd597ef7c9b
root <bono39@yandex.ru>
2023-01-16 22:41:47 +0300

    add repository

--- Chapter_1/Tangy/TangyWeb_Server/Program.cs
+++ Chapter_1/Tangy/TangyWeb_Server/Program.cs
@@ -2,6 +2,8 @@ using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.EntityFrameworkCore;
 using Tandy_DataAccess.Data;
+using Tangy_Business.Repository;
+using Tangy_Business.Repository.IRepository;
 using TangyWeb_Server.Data;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -11,6 +13,8 @@ builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
---
+++ Chapter_1/Tangy/Tangy_Business/Repository/CategoryRepository.cs
@@ -0,0 +1,92 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tandy_DataAccess;
+using Tandy_DataAccess.Data;
+using Tangy_Business.Repository.IRepository;
+using Tangy_Models;
+
+namespace Tangy_Business.Repository
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        private readonly ApplicationDbContext _db;
+        private readonly IMapper _mapper;
+
+        public CategoryRepository(ApplicationDbContext db, IMapper mapper)
+        {
+            _db = db;
+            _mapper = mapper;
+        }
+
+        public CategoryDTO Create(CategoryDTO objDTO)
+        {
+            //Category category = new Category()
+            //{
+            //    Id = objDTO.Id,
+            //    Name = objDTO.Name,
+            //    CreatedDate = DateTime.Now
+            //};
+            //var category = _mapper.Map<CategoryDTO, Category>(objDTO);
+            //_db.Categories.Add(category);
+            //_db.SaveChanges();
+            //return new CategoryDTO()
+            //{
+            //    Id = category.Id,
+            //    Name = category.Name
+            //};
+
+            //return _mapper.Map<Category, CategoryDTO>(category);
+
+            var obj = _mapper.Map<CategoryDTO, Category>(objDTO);
+            obj.CreatedDate = DateTime.Now;
+
+            var addedObj = _db.Categories.Add(obj);
+            _db.SaveChanges();
+            
+            return _mapper.Map<Category, CategoryDTO>(addedObj.Entity);
+        }
+
+        public int Delete(int id)
+        {
+            var obj = _db.Categories.FirstOrDefault(x => x.Id == id);
+            if (obj != null)
+            {
+                _db.Categories.Remove(obj);
+                return _db.SaveChanges();
+            }
+            return 0;
+        }
+
+        public CategoryDTO Get(int id)
+        {
+            var obj = _db.Categories.FirstOrDefault(x => x.Id == id);
+            if (obj != null)
+            {
+                return _mapper.Map<Category, CategoryDTO>(obj);
+            }
+            return new CategoryDTO();
+        }
+
+        public IEnumerable<CategoryDTO> GetAll()
+        {
+            return _mapper.Map<IEnumerable<Category>, IEnumerable<CategoryDTO>>(_db.Categories);
+        }
+
+        public CategoryDTO Update(CategoryDTO objDTO)
+        {
+            var objFromDb = _db.Categories.FirstOrDefault(x => x.Id == objDTO.Id);
+            if (objFromDb != null)
+            {
+                objFromDb.Name = objDTO.Name;
+                _db.Categories.Update(objFromDb);
+                _db.SaveChanges();
+                return _mapper.Map<Category, CategoryDTO>(objFromDb);
+            }
+            return objDTO;
+        }
+    }
+}
