ae1e6da270ce45bdd14aae297612daa278dbac97
Umrikhin <bono39@yandex.ru>
2023-01-18 11:19:35 +0300

    Add Chapter 2

---
+++ Chapter_2/Tangy/TangyWeb_Server/wwwroot/js/command.js
@@ -0,0 +1,25 @@
+﻿window.ShowToastr = (type, message) => {
+    if (type === "success") {
+        toastr.success(message, 'Операция произошла успешно', { timeOut: 5000 });
+    }
+    if (type === "error") {
+        toastr.error(message, 'Операция не удалась', { timeOut: 5000 });
+    }
+}
+
+window.ShowSweetAlert = (type, message) => {
+    if (type === "success") {
+        Swal.fire(
+            'Уведомление об успехе!',
+            message,
+            'success'
+        )
+    }
+    if (type === "error") {
+        Swal.fire(
+            'Уведомление об ошибке!',
+            message,
+            'error'
+        )
+    }
+}
